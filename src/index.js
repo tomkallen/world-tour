5a019ea0ac2e242961d8cebc695cf365d02364dd
tomkallen <spbeat@mail.ru>
2016-12-07 20:28:51 +0300

    add router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    Router,
+    Route,
+    IndexRedirect,
+    browserHistory
+} from 'react-router';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render( <
+    App / > ,
+    document.getElementById('root')
 );
