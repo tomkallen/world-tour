370d32d727b879de7648ad6145803bfb8af5b8b4
tomkallen <spbeat@mail.ru>
2016-12-11 11:23:42 +0300

    missing files

---
+++ src/components/info/info.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+export class Info extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className="info">I am Info</div>
+        )
+    }
+}
