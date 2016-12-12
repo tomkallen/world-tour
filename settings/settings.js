230c1c85bfd8ad44ddefca70b97983cd2303bbca
tomkallen <spbeat@mail.ru>
2016-12-12 12:14:35 +0300

    better eject

--- settings/settings.js
+++
@@ -1,8 +0,0 @@
-var Settings = {
-    port: process.env.PORT || 3333,
-    serverResponse: "***\nСТАТУС СЕРВЕРА: OK. ПОРТ: ",
-    MOTD: "\nMessage of the day goes here",
-    secret: "i am gonna hash my strings up"
-};
-
-module.exports = Settings;
