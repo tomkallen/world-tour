64181c60a0d21003b8c74bd0df45fde00789361b
tomkallen <spbeat@mail.ru>
2016-12-15 00:06:57 +0300

    style fixes

--- src/components/visual/continent-info.js
+++ src/components/visual/continent-info.js
@@ -3,25 +3,21 @@ import '../info/info.css';
 
 
 export class ContinentInfo extends Component {
-    constructor(props){
-        super(props);
 
-        // filtering raw database to pull the needed info
-        // `item.continentName !== item.countryName` is Antarctica hack
-
-
-    }
     render(){
+
+        // filtering sliced clones of db and converting data to numeric values.
+        // Just in case.
         let filteredDB = this.props.data.filter( item =>
         item.continentName === this.props.continent && item.continentName !== item.countryName);
         let largest = filteredDB.slice(0).sort( (a,b) => {
-            if(+a.areaInSqKm < +b.areaInSqKm) return -1;
+            if (+a.areaInSqKm < +b.areaInSqKm) return -1;
             if(+a.areaInSqKm > +b.areaInSqKm) return 1;
             return 0;
         });
         let populated = filteredDB.slice(0).sort( (a,b) => {
-            if(+a.population < +b.population) return -1;
-            if(+a.population > +b.population) return 1;
+            if (+a.population < +b.population) return -1;
+            if (+a.population > +b.population) return 1;
             return 0;
         });
 
@@ -35,8 +31,8 @@ export class ContinentInfo extends Component {
                     And <span>{ largest[0].countryName }</span> is the tiniest. It is just { largest[0].areaInSqKm } &#13218; big!
                     Or should we say 'small'?
                 </p>
-                <p>A lot of people live in <span>{populated[populated.length-1].countryName}</span>!
-                    Something like {populated[populated.length-1].population} to be exact!
+                <p>A lot of people live in <span>{ populated[populated.length-1].countryName }</span>!
+                    Something like { populated[populated.length-1].population } to be exact!
                 </p>
             </div>
         )
